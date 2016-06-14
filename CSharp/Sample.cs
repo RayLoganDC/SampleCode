703bb6ce00acfbba77a19dd0d45590c9b5a1b4d4
Kiran Sankuru <ksankuri@gmail.com>
2016-06-13 22:20:54 -0400

    a

---
+++ CSharp/Sample.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.AspNet.Builder;
+
+
+namespace SampleNamespace
+{
+    public class SampleClass
+    {
+	   int a = 1;
+       int b = 2;
+       int c;
+      
+       c = a * b;
+       console.log(c); 
+    }
+    
+    public class TestClass
+    {
+    /*
+        // Place your snippets for C# here. Each snippet is defined under a snippet name and has a prefix, body and 
+        // description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+        // $1, $2 for tab stops, ${id} and ${id:label} and ${1:label} for variables. Variables with the same id are connected.
+        // Example:
+        "Print to console": {
+            "prefix": "log",
+            "body": [
+                "console.log('$1');",
+                "$2"
+            ],
+            "description": "Log output to console"
+        }
+    */
+    }
+}
+
+
